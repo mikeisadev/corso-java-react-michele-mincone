79caf26c43f582f85ca485fd5945dbe27b9c4267
Michele Mincone <102952044+mikeisadev@users.noreply.github.com>
2025-11-08 21:41:15 +0000

    Scanner class in Java, prendere dati in input nella console

---
+++ 1) Basi del linguaggio Java/JavaScanner.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; 
+
+public class JavaScanner {
+	
+    /**
+     * In Java posso importare "java.util.Scanner" in alto e ottenere dati dalla console inserendoli come input.
+     * 
+     * Crea un nuovo file Java dentro Eclipse.
+     * 
+     * Poi copia e incolla questo codice e vedi che succede!
+     * 
+     * Buon coding!
+     */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String nome = input.nextLine();
+		
+		System.out.print("Inserisci il tuo cognome: ");
+		
+		String cognome = input.nextLine();
+		
+		System.out.print("Il tuo nome è: " + nome + ' ' + cognome);
+	}
+
+}
