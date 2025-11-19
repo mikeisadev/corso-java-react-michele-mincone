58051cff0d6e5ef5df78871b72ab4e35a5c26acd
Michele Mincone <102952044+mikeisadev@users.noreply.github.com>
2025-11-19 10:01:48 +0000

    Aggiustare nome classe RubricaNormale in Rubrica

--- 1) Basi del linguaggio Java/eserciziSvolti/Rubrica.java
+++ 1) Basi del linguaggio Java/eserciziSvolti/Rubrica.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
 
-public class RubricaNormale {
+public class Rubrica {
 	
 	private static Map<String, String> rubrica = new HashMap<>();
 	
